29e47a0f4eabeda02745bb57bd696de29321dd41
Ishu20002222 <ishitaa20@iitk.ac.in>
2023-08-01 10:04:49 +0530

    ye lo

--- hamiltonian_gpu.py
+++ hamiltonian_gpu.py
@@ -38,11 +38,11 @@ complex_const = -1j
 # #length of inter would be number of edges and would be labelled based on temp
 
 
-# j = [1,1,1,1,1,1]
-# omega = -20
-# rabif = [0,0,0,0,0,0,omega]
-# detun = [2*j[0], 2*j[1], 2*j[2], 2*j[3], 2*j[4], 2*j[5], 2*(j[0]+j[1]+j[2]+j[3]+j[4]+j[5])]
-# inter = [4*j[0], 4*j[1], 4*j[2], 4*j[3], 4*j[4], 4*j[5]] 
+j = [1,1,1,1,1,1]
+omega = -20
+rabif = [0,0,0,0,0,0,omega]
+detun = [2*j[0], 2*j[1], 2*j[2], 2*j[3], 2*j[4], 2*j[5], 2*(j[0]+j[1]+j[2]+j[3]+j[4]+j[5])]
+inter = [4*j[0], 4*j[1], 4*j[2], 4*j[3], 4*j[4], 4*j[5]] 
 
 # j = [1,1,1,1,1,1,1,1]
 # omega = -20
@@ -50,13 +50,13 @@ complex_const = -1j
 # detun = [2*j[0], 2*j[1], 2*j[2], 2*j[3], 2*j[4], 2*j[5], 2*j[6], 2*j[7], 2*(j[0]+j[1]+j[2]+j[3]+j[4]+j[5]+j[6]+j[7])]
 # inter = [4*j[0], 4*j[1], 4*j[2], 4*j[3], 4*j[4], 4*j[5], 4*j[6], 4*j[7]]
 
-j = [1,1,1,1,1,1,1,1,1,1]
-omega = -20
-rabif = [0,0,0,0,0,0,0,0,0,0,omega]
-detun = [2*j[0], 2*j[1], 2*j[2], 2*j[3], 2*j[4], 2*j[5], 2*j[6], 2*j[7], 2*j[8], 2*j[9], 2*(j[0]+j[1]+j[2]+j[3]+j[4]+j[5]+j[6]+j[7]+j[8]+j[9])]
-inter = [4*j[0], 4*j[1], 4*j[2], 4*j[3], 4*j[4], 4*j[5], 4*j[6], 4*j[7], 4*j[8], 4*j[9]]
+# j = [1,1,1,1,1,1,1,1,1,1]
+# omega = -20
+# rabif = [0,0,0,0,0,0,0,0,0,0,omega]
+# detun = [2*j[0], 2*j[1], 2*j[2], 2*j[3], 2*j[4], 2*j[5], 2*j[6], 2*j[7], 2*j[8], 2*j[9], 2*(j[0]+j[1]+j[2]+j[3]+j[4]+j[5]+j[6]+j[7]+j[8]+j[9])]
+# inter = [4*j[0], 4*j[1], 4*j[2], 4*j[3], 4*j[4], 4*j[5], 4*j[6], 4*j[7], 4*j[8], 4*j[9]]
 
-N1 = 11
+N1 = 7
 # Initialize the resulting matrix as zero
 matrix = torch.zeros((2**N1, 2**N1), dtype=torch.complex128)
 matrix2 = torch.zeros((2**N1, 2**N1), dtype=torch.complex128)
@@ -66,6 +66,14 @@ ham = torch.zeros((2**N1, 2**N1), dtype=torch.complex128)
 s = torch.tensor([[0, 1], [1, 0]], dtype=torch.complex128)
 p = torch.tensor([[1, 0], [0, 1]], dtype=torch.complex128)
 n = torch.tensor([[0, 0], [0, 1]], dtype=torch.complex128)
+
+matrix = matrix.to(device)
+matrix2 = matrix2.to(device)
+matrix3 = matrix3.to(device)
+ham = ham.to(device)
+s = s.to(device)
+p = p.to(device)
+n = n.to(device)
 
 # Loop over all combinations
 for j in range(N1):
@@ -100,15 +108,19 @@ for j in range(N1-1):
 
 def get_U(a,b,g):
     
-    N1 = 11
+    N1 = 7
     pauli_x = torch.tensor([[0., 1.], [1., 0.]], dtype=torch.complex128)
     identity = torch.tensor([[1., 0.], [0., 1.]], dtype=torch.complex128)
     pauli_z = torch.tensor([[1., 0.], [0., -1.]], dtype=torch.complex128)
+    pauli_x = pauli_x.to(device)
+    identity = identity.to(device)
+    pauli_z = pauli_z.to(device)
 
     # u1 = torch.zeros((2**N1, 2**N1), dtype=torch.complex128)
     # u2 = torch.zeros((2**N1, 2**N1), dtype=torch.complex128)
     # u3 = torch.zeros((2**N1, 2**N1), dtype=torch.complex128)
     U = torch.eye(2**N1, dtype=torch.complex128)
+    U = U.to(device)
 
     for i in range(N1):
         pz = []
@@ -120,19 +132,24 @@ def get_U(a,b,g):
             px.append(x)
         result_z = reduce(torch.kron, pz)
         result_x = reduce(torch.kron, px)
+        result_z = result_z.to(device)
+        result_x = result_x.to(device)
         # u1 = torch.tensor(-1j * a[i] * result_z).matrix_exp()
         # u2 = torch.tensor(-1j * b[i] * result_x).matrix_exp()
         # u3 = torch.tensor(-1j * g[i] * result_z).matrix_exp()
-        # u1 = torch.cos((a[i]))*torch.eye(2**N1) - 1j *torch.sin((a[i]))*result_z
-        # u2 = torch.cos((b[i]))*torch.eye(2**N1) - 1j *torch.sin((b[i]))*result_x
-        # u3 = torch.cos((g[i]))*torch.eye(2**N1) - 1j *torch.sin((g[i]))*result_z
-        # U = u3 @ u2 @ u1 @ U
-        U = torch.matmul((torch.cos((g[i]))*torch.eye(2**N1) - 1j *torch.sin((g[i]))*result_z),torch.matmul((torch.cos((b[i]))*torch.eye(2**N1) - 1j *torch.sin((b[i]))*result_x),torch.matmul((torch.cos((a[i]))*torch.eye(2**N1) - 1j *torch.sin((a[i]))*result_z),U)))
+        u1 = torch.cos((a[i]))*torch.eye(2**N1).to(device) - 1j *torch.sin((a[i]))*result_z
+        u2 = torch.cos((b[i]))*torch.eye(2**N1).to(device) - 1j *torch.sin((b[i]))*result_x
+        u3 = torch.cos((g[i]))*torch.eye(2**N1).to(device) - 1j *torch.sin((g[i]))*result_z
+        u1 = u1.to(device)
+        u2 = u2.to(device)
+        u3 = u3.to(device)
+        U = u3 @ u2 @ u1 @ U
+        # U = torch.matmul((torch.cos((g[i]))*torch.eye(2**N1) - 1j *torch.sin((g[i]))*result_z),torch.matmul((torch.cos((b[i]))*torch.eye(2**N1) - 1j *torch.sin((b[i]))*result_x),torch.matmul((torch.cos((a[i]))*torch.eye(2**N1) - 1j *torch.sin((a[i]))*result_z),U)))
 
     return U
 
 def evolution(time, params,l):
-    N1 = 11
+    N1 = 7
     L = 4
     a = params[0]
     b = params[1]
@@ -145,9 +162,11 @@ def evolution(time, params,l):
     # ham_np = ham.numpy()
     # time = 0.01
     H = ham.matrix_exp()
+    H = H.to(device)
     # H = get_H(time)
 
     U_final = torch.eye(2**N1, dtype=torch.complex128)
+    U_final = U_final.to(device)
     # U1 = get_U(a[l][0],b[l][0],g[l][0])
     # U2 = get_U(a[l][1],b[l][1],g[l][1])
     # pdb.set_trace()
@@ -156,6 +175,8 @@ def evolution(time, params,l):
     for l in range(L):
         U1 = get_U(a[l][0],b[l][0],g[l][0])
         U2 = get_U(a[l][1],b[l][1],g[l][1])
+        U1 = U1.to(device)
+        U2 = U2.to(device)
         # pdb.set_trace()
         # U_result = torch.matmul(torch.matmul(U2, H), U1)
         # U_final = torch.matmul(U_result, U_final)
@@ -164,13 +185,13 @@ def evolution(time, params,l):
     return U_final
 
 def expectation(state,time,params):
-    N1 = 11
+    N1 = 7
     # pdb.set_trace()
-    L=4
+    L = 4
     # state = state.reshape(4, 4)
     # state = torch.flatten(torch.kron(state, torch.tensor([1.,0.])))
-    # state = state.to(device)
-    state = (torch.kron(state, torch.tensor([1.,0.])))
+    state = state.to(device)
+    state = (torch.kron(state, torch.tensor([1.,0.]).to(device)))
     # for l in range(L):
     U = evolution(time,params,L)
     state = U @ state.view(-1,state.size()[0])
@@ -212,10 +233,13 @@ class QuantumPerceptron(nn.Module):
         # self.layer2 = nn.Linear(hidden_size, output_size)
         # self.layer1 = nn.Linear(input_size, output_size)
         L = 4
-        N1 = 11
+        N1 = 7
         a = torch.normal(mean=0.0, std=1., size=[L,2,N1])
         b = torch.normal(mean=0.0, std=1., size=[L,2,N1])
         g = torch.normal(mean=0.0, std=1., size=[L,2,N1])
+        a = a.to(device)
+        b = b.to(device)
+        g = g.to(device)
         self.params = nn.Parameter((torch.stack((a,b,g))))
         self.params.requires_grad = True
 
