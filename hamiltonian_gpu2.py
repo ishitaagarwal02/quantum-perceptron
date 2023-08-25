279303f6fc410b756f0bc99ad3e966419f6a31f7
Ishu20002222 <ishitaa20@iitk.ac.in>
2023-08-25 12:50:48 +0530

    U try

--- hamiltonian_gpu2.py
+++ hamiltonian_gpu2.py
@@ -262,7 +262,7 @@ model = model
 # Initialize parameters with Gaussian distribution centered at 0
 # import matplotlib.pyplot as plt
 # Set a seed for reproducibility
-torch.manual_seed(10000)
+torch.manual_seed(100)
 
 x = Z2DatasetLoader()
 y = Z3DatasetLoader()
